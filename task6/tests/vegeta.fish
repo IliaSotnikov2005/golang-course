9c567ff98dee5dc51aef7bd2cfe0d70570b2c2c6
Ilya Sotnikov <ilia12st@yandex.ru>
2026-05-04 14:50:10 +0300

    update vegeta script

--- task6/tests/vegeta.fish
+++ task6/tests/vegeta.fish
@@ -1,6 +1,2 @@
 #!/usr/bin/fish
-echo "Test run: RPS=20 Duration=5s"
-echo "Results:"
-echo "GET http://localhost:8080/api/v1/subscriptions/info" | \
-vegeta attack -rate=20 -duration=5s | \
-vegeta report -type=json | jq '.status_codes'
+echo "GET http://localhost:8080/api/v1/subscriptions/info" | vegeta attack -rate=20 -duration=5s | vegeta report
